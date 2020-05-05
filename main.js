328a1177d4d7a3076f72b53426fd4790491f88f9
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-05-05 15:55:31 -0500

    Updated File

---
+++ main.js
@@ -0,0 +1,6 @@
+separateWords = (str) => {
+    let words = str.split(" ");
+    console.log("There are " + words.length + " words.");
+}
+
+separateWords("I love vanilla JavaScript and arrow functions so much!"); // There are 9 words.
