61e0466c05d48c1f32f9da6be5b74ad0178406ba
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-05-15 06:59:58 -0500

    Updated File

--- main.js
+++ main.js
@@ -1,44 +1,18 @@
-// Setup
-var collection = {
-  2548: {
-    album: "Slippery When Wet",
-    artist: "Bon Jovi",
-    tracks: [
-      "Let It Rock",
-      "You Give Love a Bad Name"
-    ]
-  },
-  2468: {
-    album: "1999",
-    artist: "Prince",
-    tracks: [
-      "1999",
-      "Little Red Corvette"
-    ]
-  },
-  1245: {
-    artist: "Robert Palmer",
-    tracks: []
-  },
-  5439: {
-    album: "ABBA Gold"
-  }
-};
+// letsBegin = () => {
+//     alert("We are going to begin.");
+// }
 
-// Only change code below this line
-function updateRecords(id, prop, value) {
-  if (value === "") {
-    delete collection[id][prop];
-  } else if (prop !== "tracks" && value) {
-    collection[id][prop] = value;
-  } else if (prop === "tracks" && (collection[id].hasOwnProperty("tracks") === false)) {
-    collection[id]["tracks"] = [];
-    collection[id]["tracks"].push(value);
-  } else if (prop === "tracks" && value) {
-    collection[id][prop].push(value);
-  }
+// letsBegin();
 
-  return collection;
+startNow = () => {
+    console.log("starting now...");
 }
 
-updateRecords(5439, "artist", "ABBA");
+startNow();
+
+changeTheDOM = () => {
+    let myVar = document.getElementById("thisOne");
+    alert(myVar.innerHTML);
+}
+
+changeTheDOM();
