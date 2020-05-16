936d1e32873b44a7251162abc8ef33de6c987911
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-05-15 23:31:23 -0500

    Updated File

---
+++ test/JavaScript/main.js
@@ -0,0 +1,14 @@
+// #myDiv
+
+function changeBg() {
+    let myValue = document.getElementById("myDiv");
+    myValue.style.backgroundColor = "white";
+}
+function changeBg1() {
+    let myValue = document.getElementById("myDiv1");
+    myValue.style.backgroundColor = "orange";
+}
+function changeBg2() {
+    let myValue = document.getElementById("myDiv2");
+    myValue.style.backgroundColor = "cyan";
+}
