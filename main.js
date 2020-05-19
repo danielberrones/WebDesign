c0ff33ef21379b2237521687cd83549cb1d225ff
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-05-18 23:10:23 -0500

    Updated File

---
+++ main.js
@@ -0,0 +1,10 @@
+// function countArray() {
+//     const myArray = [1,43,5,7,44,2,86,62,true,null,undefined,{},[],7];
+//     myArray.forEach(i => console.log(i));
+//     console.log("array length is "+myArray.length);
+// }
+
+// countArray();
+
+
+
