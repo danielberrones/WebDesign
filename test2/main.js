eec297c6a232e9559aed0646856730e3c1b1cd69
Daniel Berrones <daniel.a.berrones@gmail.com>
2020-05-28 18:15:00 -0500

    Updated Code

---
+++ test2/main.js
@@ -0,0 +1,20 @@
+function natoAlphabet(letter) {
+	switch(letter) {
+		case "a":
+			console.log("alpha");
+			break;
+		case "b":
+			console.log("bravo");
+			break;
+		case "c":
+			console.log("charlie");
+			break;
+		default:
+			console.log("default");
+			break;
+	}
+}
+
+natoAlphabet("c");
+natoAlphabet("a");
+natoAlphabet("b");
